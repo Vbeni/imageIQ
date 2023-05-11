f320b83f387f9b677daf5ed8dee2f4677afb740b
Vbeni <benivillafana@gmail.com>
2023-05-11 17:20:41 -0400

    started server/imported middleware

---
+++ server.js
@@ -0,0 +1,24 @@
+//env variable
+require("dotenv").config();
+//port from env
+const { PORT } = process.env;
+//import express
+const express = require("express");
+//create app object
+const app = express();
+
+//import middleware
+const cors = require('cors');
+const morgan = require('morgan');
+
+//middleware
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+//test route
+app.get('/', (req, res) =>{
+    res.send("hello world");
+});
+//listener
+app.listen(PORT, () => console.log(`Forever listening to the one the only PORT ${PORT}`));
