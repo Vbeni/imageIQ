695496b2ceed8537c3f4aa414e1e5a49b9c09b21
Vbeni <benivillafana@gmail.com>
2023-05-11 20:02:50 -0400

    updated User model for user auth

--- models/User.js
+++ models/User.js
@@ -1,11 +1,20 @@
 const mongoose = require('./connection');
 
 const userSchema = new mongoose.Schema({
-    name: {
+    username: {
         type: String,
         required: true,
-    }
-})
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+},
+{
+    timestamps: true,
+}
+);
 
 const User = mongoose.model("User", userSchema);
 
