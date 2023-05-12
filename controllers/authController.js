de4db1e4cd2b50a6ff2b19d8051f847892ac29e8
Vbeni <benivillafana@gmail.com>
2023-05-11 20:21:54 -0400

    layout for post register/login route

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,4 +4,8 @@ const bcrypt = require("bcrypt");
 
 const router = express.Router();
 
+router.post("/register", async (req, res, next) =>{});
+
+router.post("/login", async (req, res, next) =>{})
+
 module.exports = router;
