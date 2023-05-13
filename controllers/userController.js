8ae25dfa36366d01b8ea5eb8c331da72685ff4f8
Vbeni <benivillafana@gmail.com>
2023-05-13 17:47:50 -0400

    user delete route

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,4 +20,13 @@ router.put("/:id", async (req, res) =>{
         res.status(400).json(error);
     }
 });
+
+router.delete("/:id", async (req, res) => {
+    try {
+        res.json(
+            await User.findByIdAndRemove(req.params.id));
+    }catch(error) {
+        res.status(400).json(error);
+    }
+});
 module.exports = router;
