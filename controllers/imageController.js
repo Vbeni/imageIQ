e3a07a7da7608f558ce06fbbb50ad3deda574ca3
Vbeni <benivillafana@gmail.com>
2023-05-15 19:01:47 -0400

    finished remaining image routes

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,14 +1,48 @@
 const express = require('express')
+const Image = require("../models/Image")
 const router = express.Router()
 
 //index 
 router.get("/", async (req, res) => {
-    res.status(200).json({message: 'image index route'})
+    try {
+        res.json(await Image.find({}));
+    }catch(error){
+        res.status(400).json(error);
+    }
 });
 
 //create route 
 router.post("/", async (req, res) => {
-    res.status(201).json({message: 'image create route'})
+   try{
+    res.json(await Image.create(req.body));
+   }catch (error){
+    res.status(400).json(error);
+   }
 });
 
+router.get("/:id", async (req, res) => {
+    try{
+        res.json(await Image.findById)(req.params.id)
+    }catch(error){
+        res.status(400).json(error);
+    }
+})
+
+router.put("/:id", async (req, res) =>{
+    try{
+        res.json(
+            await Image.findByIdAndUpdate(req.params.id, req.body, { new: true})
+        );
+    }catch(error){
+        res.status(400).json(error);
+    }
+});
+router.delete("/:id", async (req, res) => {
+    try {
+        res.json(
+            await Image.findByIdAndRemove(req.params.id));
+    }catch(error) {
+        res.status(400).json(error);
+    }
+});
 module.exports = router
