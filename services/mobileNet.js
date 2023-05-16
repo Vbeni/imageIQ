83fa37d7a155f45d9d8cd0182d50cf0e95f4fbd3
Vbeni <benivillafana@gmail.com>
2023-05-16 19:44:07 -0400

    mobilenet classification funtion & exported to image controllers

--- services/mobileNet.js
+++ services/mobileNet.js
@@ -1,2 +1,20 @@
 const mobilenet = require('@tensorflow-models/mobilenet');
 
+let model; 
+mobilenet.load().then(mobileNetModel => {
+    model = mobileNetModel;
+}).catch(err => console.error(err));
+
+async function classifyImage(image){
+    if (!model){
+        throw new Error('Model is not loaded');
+    }
+
+    const predictions = await model.classify(image);
+    return predictions;
+}
+
+module.exports = {
+    classifyImage,
+    model
+}
